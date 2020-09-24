b2876a44195fe9cfb88df57902131431d2831330
Justin Ethier <justin.ethier@gmail.com>
2020-09-23 22:30:42 -0400

    Obsolete

--- test-r7rs.scm
+++
@@ -1,9 +0,0 @@
-(import (scheme base)
-        (scheme process-context)
-        (scheme write)
-        (cyclone test)
-        (srfi 197)
-        (srfi 2))
-
-;(include "./srfi-197.scm")
-(include "./test.scm")
