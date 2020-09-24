32ea797268f902dfe348e8cdd95d008bc1491616
Justin Ethier <justin.ethier@gmail.com>
2020-09-23 22:35:18 -0400

    Initial file

---
+++ package.scm
@@ -0,0 +1,17 @@
+(package
+ (name           srfi-197)
+ (version        0.1)
+ (license        "MIT")
+ (authors        "Adam Nelson"
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "Pipeline Operators")
+ (tags           "srfi" "language extensions")
+ (docs           "https://github.com/cyclone-scheme/cyclone-winds/wiki/srfi-197")
+ (test           "tests.scm")
+ (test-dependencies ())
+ (foreign-dependencies ())
+
+ (library
+     (name (srfi 197))
+   (description "Pipeline Operators")))
+
